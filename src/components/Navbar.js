b2b974808151ab9251ebc45de469cd35ddc71518
Arttu Hartikainen <arttuhartikainen@Arttu-MacBook-Pro.local>
2021-01-16 15:41:34 +0200

    Navbar layout done

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,6 +5,7 @@ import Toolbar from "@material-ui/core/Toolbar";
 import Box from "@material-ui/core/Box";
 import Input from "@material-ui/core/Input";
 import Link from "@material-ui/core/Link";
+import Button from "@material-ui/core/Button";
 
 const useStyles = makeStyles(theme => ({
 	navbarContainer: {
@@ -14,6 +15,7 @@ const useStyles = makeStyles(theme => ({
 		fontFamily: "sans-serif",
 	},
 	navbarIcon: {
+		color: "#fff",
 		fontSize: "2.1rem",
 		paddingLeft: "0.5rem",
 		"&:hover": {
@@ -39,14 +41,53 @@ const useStyles = makeStyles(theme => ({
 			paddingLeft: "12px",
 		},
 	},
-	navbarLinks: {},
+	navbarLinks: {
+		flex: 1,
+	},
 	linksItem: {
 		paddingLeft: "1rem",
 		color: "#fff",
 		fontWeight: "bold",
+		cursor: "pointer",
 		"&:hover": {
 			color: "#cccdce",
 		},
+	},
+	navbarMenuNew: {
+		width: "40px",
+	},
+	menuNewButton: {
+		color: "#fff",
+		fontSize: "20px",
+		cursor: "pointer",
+		"&:hover": {
+			color: "#cccdce",
+		},
+	},
+	menuNewIcon: {
+		fontSize: "12px",
+		paddingLeft: "4px",
+		paddingTop: "6px",
+	},
+	navbarMenuProfile: {
+		width: "45px",
+	},
+	menuProfileButton: {
+		cursor: "pointer",
+		color: "#fff",
+		"&:hover": {
+			color: "#cccdce",
+		},
+	},
+	buttonAvatar: {
+		width: "20px",
+		height: "20px",
+		borderRadius: "50%",
+	},
+	menuProfileIcon: {
+		fontSize: "12px",
+		paddingLeft: "4px",
+		paddingTop: "6px",
 	},
 }));
 
@@ -56,9 +97,9 @@ function Navbar(props) {
 		<React.Fragment>
 			<AppBar className={classes.navbarContainer}>
 				<Toolbar>
-					<Box className={classes.navbarIcon}>
+					<Link to="/" className={classes.navbarIcon}>
 						<i class="fab fa-github"></i>
-					</Box>
+					</Link>
 					<Box className={classes.navbarSearch}>
 						<Input
 							className={classes.searchInput}
@@ -80,6 +121,27 @@ function Navbar(props) {
 						<Link to="/" underline="none" className={classes.linksItem}>
 							Explore
 						</Link>
+					</Box>
+
+					<Box className={classes.navbarMenuNew}>
+						<Button className={classes.menuNewButton} disableRipple={true}>
+							+
+							<Box className={classes.menuNewIcon}>
+								<i class="fas fa-caret-down"></i>
+							</Box>
+						</Button>
+					</Box>
+					<Box className={classes.navbarMenuProfile}>
+						<Button className={classes.menuProfileButton} disableRipple={true}>
+							<img
+								src="/images/avatar.png"
+								alt="avatar"
+								className={classes.buttonAvatar}
+							/>
+							<Box className={classes.menuProfileIcon}>
+								<i class="fas fa-caret-down"></i>
+							</Box>
+						</Button>
 					</Box>
 				</Toolbar>
 			</AppBar>
