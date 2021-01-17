aa39362f4807fc55668491b9ac836ad0e6bbc241
Arttu Hartikainen <arttuhartikainen@Arttu-MacBook-Pro.local>
2021-01-17 12:55:53 +0200

    Dropdowns for New menu and Profile menu

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,6 +6,9 @@ import Box from "@material-ui/core/Box";
 import Input from "@material-ui/core/Input";
 import Link from "@material-ui/core/Link";
 import Button from "@material-ui/core/Button";
+import Menu from "@material-ui/core/Menu";
+import MenuItem from "@material-ui/core/MenuItem";
+import Divider from "@material-ui/core/Divider";
 
 const useStyles = makeStyles(theme => ({
 	navbarContainer: {
@@ -13,11 +16,13 @@ const useStyles = makeStyles(theme => ({
 		height: "60px",
 		justifyContent: "center",
 		fontFamily: "sans-serif",
+		boxShadow: "none",
 	},
 	navbarIcon: {
 		color: "#fff",
 		fontSize: "2.1rem",
 		paddingLeft: "0.5rem",
+		cursor: "pointer",
 		"&:hover": {
 			color: "#cccdce",
 		},
@@ -55,6 +60,7 @@ const useStyles = makeStyles(theme => ({
 	},
 	navbarMenuNew: {
 		width: "40px",
+		marginRight: "5px",
 	},
 	menuNewButton: {
 		color: "#fff",
@@ -69,8 +75,19 @@ const useStyles = makeStyles(theme => ({
 		paddingLeft: "4px",
 		paddingTop: "6px",
 	},
+	menuNewItem: {
+		padding: "4px 8px 4px 16px",
+		fontSize: "14px",
+		color: "#24292e",
+		transition: "none",
+		"&:hover": {
+			backgroundColor: "#036dd6",
+			color: "#fff",
+		},
+	},
 	navbarMenuProfile: {
 		width: "45px",
+		marginRight: "5px",
 	},
 	menuProfileButton: {
 		cursor: "pointer",
@@ -89,10 +106,80 @@ const useStyles = makeStyles(theme => ({
 		paddingLeft: "4px",
 		paddingTop: "6px",
 	},
+	profileLinkContainer: {
+		width: "100%",
+		justifyContent: "center",
+		textAlign: "center",
+		paddingBottom: "8px",
+		outline: "none",
+	},
+	profileLink: {
+		cursor: "pointer",
+		color: "#24292e",
+	},
+	statusButton: {
+		width: "150px",
+		height: "30px",
+		marginLeft: "15px",
+		marginTop: "8px",
+		marginBottom: "8px",
+		borderRadius: "6px",
+		textTransform: "none",
+		fontSize: "12px",
+		justifyContent: "flex-start",
+		paddingLeft: "8px",
+		color: "#586069",
+		border: "1px solid #e1e4e8",
+		transition: "none",
+		"&:hover": {
+			backgroundColor: "transparent",
+			color: "#0366d6",
+		},
+	},
+	statusIcon: {
+		fontSize: "16px",
+		paddingRight: "8px",
+	},
+	menuProfileItem: {
+		padding: "4px 8px 4px 16px",
+		fontSize: "14px",
+		color: "#24292e",
+		transition: "none",
+		"&:hover": {
+			backgroundColor: "#036dd6",
+			color: "#fff",
+		},
+	},
+	profileItemsPrimary: {
+		marginTop: "8px",
+		marginBottom: "8px",
+	},
+	profileItemsSecondary: {
+		marginTop: "8px",
+	},
 }));
 
-function Navbar(props) {
+function Navbar() {
 	const classes = useStyles();
+	const [anchorElNew, setAnchorElNew] = React.useState(null);
+	const [anchorElProfile, setAnchorElProfile] = React.useState(null);
+
+	const handleClickNew = event => {
+		setAnchorElNew(event.currentTarget);
+	};
+
+	const handleCloseNew = () => {
+		setAnchorElNew(null);
+	};
+
+	const handleClickProfile = event => {
+		setAnchorElProfile(event.currentTarget);
+	};
+
+	const handleCloseProfile = () => {
+		setAnchorElProfile(null);
+	};
+
 	return (
 		<React.Fragment>
 			<AppBar className={classes.navbarContainer}>
@@ -122,17 +209,86 @@ function Navbar(props) {
 							Explore
 						</Link>
 					</Box>
-
 					<Box className={classes.navbarMenuNew}>
-						<Button className={classes.menuNewButton} disableRipple={true}>
+						<Button
+							className={classes.menuNewButton}
+							disableRipple={true}
+							aria-controls="newMenu"
+							aria-haspopup="true"
+							onClick={handleClickNew}
+						>
 							+
 							<Box className={classes.menuNewIcon}>
 								<i class="fas fa-caret-down"></i>
 							</Box>
 						</Button>
+						<Menu
+							className={classes.menuNewContainer}
+							id="newMenu"
+							anchorEl={anchorElNew}
+							transitionDuration={0}
+							keepMounted
+							open={Boolean(anchorElNew)}
+							onClose={handleCloseNew}
+							PaperProps={{
+								style: {
+									backgroundColor: "#fff",
+									marginTop: "32px",
+									marginLeft: "-58px",
+									width: "160px",
+									paddingTop: 0,
+									paddingBottom: 0,
+									borderRadius: "6px",
+									border: "1px solid #cccdce",
+									boxShadow: "0 8px 24px rgba(149, 157, 165, 0.2)",
+								},
+							}}
+						>
+							<MenuItem
+								className={classes.menuNewItem}
+								disableRipple={true}
+								onClick={handleCloseNew}
+							>
+								New repository
+							</MenuItem>
+							<MenuItem
+								className={classes.menuNewItem}
+								disableRipple={true}
+								onClick={handleCloseNew}
+							>
+								Import repository
+							</MenuItem>
+							<MenuItem
+								className={classes.menuNewItem}
+								disableRipple={true}
+								onClick={handleCloseNew}
+							>
+								New gist
+							</MenuItem>
+							<MenuItem
+								className={classes.menuNewItem}
+								disableRipple={true}
+								onClick={handleCloseNew}
+							>
+								New organization
+							</MenuItem>
+							<MenuItem
+								className={classes.menuNewItem}
+								disableRipple={true}
+								onClick={handleCloseNew}
+							>
+								New Project
+							</MenuItem>
+						</Menu>
 					</Box>
 					<Box className={classes.navbarMenuProfile}>
-						<Button className={classes.menuProfileButton} disableRipple={true}>
+						<Button
+							className={classes.menuProfileButton}
+							disableRipple={true}
+							aria-controls="profileMenu"
+							aria-haspopup="true"
+							onClick={handleClickProfile}
+						>
 							<img
 								src="/images/avatar.png"
 								alt="avatar"
@@ -142,6 +298,124 @@ function Navbar(props) {
 								<i class="fas fa-caret-down"></i>
 							</Box>
 						</Button>
+						<Menu
+							className={classes.menuProfileContainer}
+							id="profileMenu"
+							anchorEl={anchorElProfile}
+							transitionDuration={0}
+							keepMounted
+							open={Boolean(anchorElProfile)}
+							onClose={handleCloseProfile}
+							PaperProps={{
+								style: {
+									backgroundColor: "#fff",
+									marginTop: "32px",
+									marginLeft: "-8px",
+									width: "180px",
+									paddingTop: 0,
+									paddingBottom: 0,
+									borderRadius: "6px",
+									border: "1px solid #cccdce",
+									boxShadow: "0 8px 24px rgba(149, 157, 165, 0.2)",
+								},
+							}}
+						>
+							<Box className={classes.profileLinkContainer}>
+								<Link to="/" underline="none" className={classes.profileLink}>
+									Signed in as <b>username</b>
+								</Link>
+							</Box>
+							<Divider />
+							<Box classname={classes.statusButtonContainer}>
+								<Button
+									variant="outlined"
+									disableRipple={true}
+									className={classes.statusButton}
+									onClick={handleCloseProfile}
+								>
+									<Box className={classes.statusIcon}>
+										<i class="far fa-smile"></i>
+									</Box>
+									Set status
+								</Button>
+							</Box>
+							<Divider />
+							<Box className={classes.profileItemsPrimary}>
+								<MenuItem
+									className={classes.menuProfileItem}
+									disableRipple={true}
+									onClick={handleCloseProfile}
+								>
+									Your profile
+								</MenuItem>
+								<MenuItem
+									className={classes.menuProfileItem}
+									disableRipple={true}
+									onClick={handleCloseProfile}
+								>
+									Your repositories
+								</MenuItem>
+								<MenuItem
+									className={classes.menuProfileItem}
+									disableRipple={true}
+									onClick={handleCloseProfile}
+								>
+									Your projects
+								</MenuItem>
+								<MenuItem
+									className={classes.menuProfileItem}
+									disableRipple={true}
+									onClick={handleCloseProfile}
+								>
+									Your Stars
+								</MenuItem>
+								<MenuItem
+									className={classes.menuProfileItem}
+									disableRipple={true}
+									onClick={handleCloseProfile}
+								>
+									Your gists
+								</MenuItem>
+							</Box>
+							<Divider />
+							<Box className={classes.profileItemsSecondary}>
+								<MenuItem
+									className={classes.menuProfileItem}
+									disableRipple={true}
+									onClick={handleCloseProfile}
+								>
+									Upgrage
+								</MenuItem>
+								<MenuItem
+									className={classes.menuProfileItem}
+									disableRipple={true}
+									onClick={handleCloseProfile}
+								>
+									Feature Preview
+								</MenuItem>
+								<MenuItem
+									className={classes.menuProfileItem}
+									disableRipple={true}
+									onClick={handleCloseProfile}
+								>
+									Help
+								</MenuItem>
+								<MenuItem
+									className={classes.menuProfileItem}
+									disableRipple={true}
+									onClick={handleCloseProfile}
+								>
+									Settings
+								</MenuItem>
+								<MenuItem
+									className={classes.menuProfileItem}
+									disableRipple={true}
+									onClick={handleCloseProfile}
+								>
+									Sign out
+								</MenuItem>
+							</Box>
+						</Menu>
 					</Box>
 				</Toolbar>
 			</AppBar>
